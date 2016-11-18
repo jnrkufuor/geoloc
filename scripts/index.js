1592cf1e14d261368de30b2b27fdff8a9220b885
jnrkufuor <jaycuffs@gmail.com>
2016-11-18 16:32:31 +0000

    geo

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,54 @@
-(function() {
+//(function() {
 
-	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	var pictureSource;
-	var destinationType;
-	function onDeviceReady() {
-		pictureSource = navigator.camera.PictureSourceType;
-		destinationType = navigator.camera.DestinationType;
+//	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+var pictureSource;
+var destinationType;
+//	function onDeviceReady() {
+pictureSource = navigator.camera.PictureSourceType;
+destinationType = navigator.camera.DestinationType;
 
-		document.getElementById("capturePhoto").onclick = function() {
-			console.log("click");
-			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-				quality : 50,
-
-				destinationType : destinationType.DATA_URL
-			});
-		}
-
-	};
-
-	function onPhotoDataSuccess(imageData) {
-
-		var smallImage = document.getElementById('smallImage');
-
-		smallImage.style.display = 'block';
-
-		smallImage.src = "data:image/jpeg;base64," + imageData;
-
-	}
-
-	function onFail(message) {
-
-		alert('Failed because: ' + message);
-
-	}
+//		document.getElementById("capturePhoto").onclick = function() {
+//			console.log("click");
+//			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+//				quality : 50,
+//
+//				destinationType : destinationType.DATA_URL
+//			});
+//		}
+//
+//	};
 
 
+function onPhotoDataSuccess(imageData) {
 
-})();
+	var smallImage = document.getElementById('smallImage');
+
+	smallImage.style.display = 'block';
+
+	smallImage.src = "data:image/jpeg;base64," + imageData;
+
+}
+
+function onFail(message) {
+
+	alert('Failed because: ' + message);
+
+}
+//})();
+
+jQuery(document).ready(function ($) {
+	$('#capturePhoto').click(function (event) {
+		console.log("click");
+		navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+			quality : 50,
+
+			destinationType : destinationType.DATA_URL
+		});
+
+	});
+});
+
+
 
 jQuery(document).ready(function ($) {
 	$('#geo').click(function (event) {
