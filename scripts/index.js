5e256439d31133ed204d789fece2ab92d2e1b7bd
jnrkufuor <jaycuffs@gmail.com>
2016-11-18 16:08:34 +0000

    gd

--- scripts/index.js
+++ scripts/index.js
@@ -8,27 +8,13 @@
 		destinationType = navigator.camera.DestinationType;
 
 		document.getElementById("capturePhoto").onclick = function() {
+			console.log("click");
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 				quality : 50,
 
 				destinationType : destinationType.DATA_URL
 			});
 		}
-
-		document.getElementById("geolocationdata").addEventListener("click", function() {
-			navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-				enableHighAccuracy : true
-			});
-		});
-
-		//watchPosition
-		var watchId = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
-			timeout : 30000
-		});
-
-		document.getElementById("clearWatchbtn").addEventListener("click", function() {
-			navigator.geolocation.clearWatch(watchID);
-		});
 
 	};
 
@@ -48,23 +34,7 @@
 
 	}
 
-	///////////geolocation bit/////////////////
-	var onSuccess = function(position) {
-		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
-	};
-	// onError Callback receives a PositionError object
-	//
-	function onError(error) {
-		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-	}
-	//watchPosition
-	var onWatchSuccess = function(position) {
-		var element = document.getElementById('divWatchMeMove');
-		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
-	};
-	function onWatchError(error) {
-		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-	}
+
 
 })();
 
